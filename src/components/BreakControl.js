b5be68a775c6a78d08d3627974ddc91edefa1890
Joseph Orokpo <josephorokpo@gmail.com>
2024-08-19 13:46:25 +0100

    Add header: h1 for a better user experience

--- src/components/BreakControl.js
+++ src/components/BreakControl.js
@@ -3,6 +3,7 @@ import React from 'react';
 const BreakControl = ({ breakLength, incrementBreak, decrementBreak }) => {
   return (
     <div id="break">
+      <h1 id="header">25 + 5 Clock</h1>
       <h2 id="break-label">Break Length</h2>
       <button id="break-decrement" onClick={decrementBreak}>-</button>
       <span id="break-length">{breakLength}</span>
